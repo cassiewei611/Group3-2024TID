1bdd816471256da31a9633235e8fb78592876c81
Cassie <cassiewei06@gmail.com>
2024-11-06 13:29:33 +0100

    cassie

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import CreateEvent from "./pages/CreateEvent";
 import EventDetail from "./pages/EventDetailPage";
-import LogInPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <HomePage />
+      <EventDetail />
     </div>
   );
 }
