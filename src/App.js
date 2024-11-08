2216bd52bb663fb946d802854a0c8dd7e0f2eb76
ZebastianLundgren <Zebastian02@gmail.com>
2024-11-08 14:24:56 +0100

    Zebbe commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 // src/App.js
 import React from "react";
 import NavBar from "./components/NavBar";
-import HomePage from "./pages/HomePage";
 import CreateEvent from "./pages/CreateEvent";
 import EventDetail from "./pages/EventDetailPage";
-import LogInPage from "./pages/LoginPage";
+import EventDetailPage from "./pages/EventDetailPage";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <HomePage />
+      <CreateEvent />
     </div>
   );
 }
