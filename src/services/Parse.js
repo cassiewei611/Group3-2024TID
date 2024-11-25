a69f53546129b247553d283da8dea9baef4fca31
Cassie <cassiewei06@gmail.com>
2024-11-25 10:58:24 +0100

    fixed interestbutton

--- src/services/Parse.js
+++ src/services/Parse.js
@@ -133,7 +133,7 @@ export const checkUserInterest = async (eventId, userId) => {
         });
         query.equalTo("user_id", {
             __type: "Pointer",
-            className: "User",
+            className: "_User",
             objectId: userId,
         });
 
