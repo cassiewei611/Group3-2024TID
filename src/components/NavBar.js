9d6475fdbb58b22262621f03b6db423af015bf6c
Cassie <cassiewei06@gmail.com>
2024-12-19 13:24:26 +0100

    username

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Parse from "parse/dist/parse.min.js";
 import "./NavBar.css";
 
 const NavBar = () => {
   const navigate = useNavigate();
   const [showDropdown, setShowDropdown] = useState(false);
+  const [username, setUsername] = useState("");
+
+
+  useEffect(() => {
+    const currentUser = Parse.User.current();
+    if (currentUser) {
+      setUsername(currentUser.get("username"));
+    }
+  }, []);
 
   const handleCreateEventClick = () => {
     navigate("/create-event");
@@ -28,11 +38,11 @@ const NavBar = () => {
 
   return (
     <div className="navbar">
-      <div className="navbar-left">
-        <button className="logo-button" onClick={handleLogoButtonClick}>
-          <img src="/img/Petish_logo.png" alt="Logo" className="logo-image" />
-        </button>
-      </div>
+
+      <button className="logo-button" onClick={handleLogoButtonClick}>
+        <img src="/img/Petish_logo.png" alt="Logo" className="logo-image" />
+      </button>
+
       <div className="navbar-right">
         <button
           className="create-event-button"
@@ -40,31 +50,34 @@ const NavBar = () => {
         >
           Create Event +
         </button>
-        <div className="profile-container">
-          <button className="profile-button" onClick={handleProfileIconClick}>
+        <div className="profile-container" onClick={handleProfileIconClick}>
+          <button className="profile-button" >
             <img
               src="/img/Profile_pic.png"
               alt="Profile"
               className="button-image"
             />
           </button>
-          {showDropdown && (
-            <div className="dropdown-menu">
-              <button
-                className="dropdown-item-profile"
-                onClick={handleProfileClick}
-              >
-                Profile
-              </button>
-              <button
-                className="dropdown-item-logout"
-                onClick={handleLogOutClick}
-              >
-                Log Out
-              </button>
-            </div>
-          )}
+          {username && <div className="username">{username}</div>}
         </div>
+
+        {showDropdown && (
+          <div className="dropdown-menu">
+            <button
+              className="dropdown-item-profile"
+              onClick={handleProfileClick}
+            >
+              Profile
+            </button>
+            <button
+              className="dropdown-item-logout"
+              onClick={handleLogOutClick}
+            >
+              Log Out
+            </button>
+          </div>
+        )}
+
       </div>
     </div>
   );
